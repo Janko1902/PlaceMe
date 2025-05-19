6ec4ca3d384bc8bbee60d803bd2ac0fb6931bb7b
Janko <161398674+Janko1902@users.noreply.github.com>
2025-05-19 20:02:25 +0200

    Initial commit

---
+++ src/main/java/com/janko/placeme/PlaceMe.java
@@ -0,0 +1,16 @@
+package com.janko.placeme;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlaceMe implements ModInitializer {
+	public static final String MOD_ID = "placeme";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/janko/placeme/PlaceMeDataGenerator.java
@@ -0,0 +1,11 @@
+package com.janko.placeme;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlaceMeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
